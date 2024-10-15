d7faaed56b817ea77bc12c64be28446cbce8ee12
Lani Huang <laniywh@gmail.com>
2024-10-15 15:42:48 -0700

    chore: Remove unused import of useRouter in search component

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -9,7 +9,6 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import Button from "@/components/ui/button";
-import { useRouter } from "next/router";
 
 export default function Search() {
   const [open, setOpen] = useState(false);
